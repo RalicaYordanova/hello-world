bd424a61a2ff7d285687921428394f4d419673f3
Ralica.Yordanova <ralica.yordano@gmail.com>
2015-04-24 20:58:36 +0200

    Reversed Guess-The-Number Game Python

---
+++ ReversedGuessTheNumber.py
@@ -0,0 +1,28 @@
+import random
+
+goal = int(raw_input("Geben Sie eine Zahl von 1 bis 100 ein! \t"))
+print goal
+
+autoversuch = 50
+print "Autoversuch: \t", autoversuch
+
+lower = 0
+upper = 100
+
+
+while (autoversuch != goal):
+    if (autoversuch < goal):
+        print autoversuch
+        print "Ich brauche eine Zahl groesser", autoversuch
+        lower = autoversuch
+        autoversuch = (upper + autoversuch) / 2
+    else:
+        print autoversuch
+        print "Ich brauche eine Zahl kleiner", autoversuch
+        upper = autoversuch
+        autoversuch = (lower + autoversuch) / 2
+        
+        
+print "Richtig: ", autoversuch
+
+
