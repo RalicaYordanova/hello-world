6457d38346b1bbeee880536d9470ad5dcdf5c5a6
Ralica.Yordanova <ralica.yordano@gmail.com>
2015-04-24 21:29:43 +0200

    Localisation

--- ReversedGuessTheNumber.py
+++ ReversedGuessTheNumber.py
@@ -2,27 +2,27 @@
 # 24.04.2015 Created by Ralica Yordanova                         #
 ##################################################################
 
-goal = int(raw_input("Geben Sie eine Zahl von 1 bis 100 ein! \t"))
-print goal
+ziel = int(raw_input("Geben Sie eine Zahl von 1 bis 100 ein! \t"))
+print ziel
 
-lower = 0
-upper = 100
+unterschranke = 0
+oberschranke = 100
 
-autoversuch = (lower + upper) / 2
+autoversuch = (unterschranke + oberschranke) / 2
 print "Autoversuch: \t", autoversuch
 
 
-while (autoversuch != goal):
-    if (autoversuch < goal):
+while (autoversuch != ziel):
+    if (autoversuch < ziel):
         print autoversuch
         print "Ich brauche eine Zahl groesser", autoversuch
-        lower = autoversuch
-        autoversuch = (upper + autoversuch) / 2
+        unterschranke = autoversuch
+        autoversuch = (oberschranke + autoversuch) / 2
     else:
         print autoversuch
         print "Ich brauche eine Zahl kleiner", autoversuch
-        upper = autoversuch
-        autoversuch = (lower + autoversuch) / 2
+        oberschranke = autoversuch
+        autoversuch = (unterschranke + autoversuch) / 2
         
         
 print "Richtig: ", autoversuch
